c02e58df595936fc06aa76ba20462b66ca768760
Municipal-and-Government <saravananvlr1010@gmail.com>
2026-09-02 15:58:08 +0530

    Update bakery layout and godaddy build assets

--- SriDeviSnacks/frontend/src/components/bakery_billing/BakeryLayout.tsx
+++ SriDeviSnacks/frontend/src/components/bakery_billing/BakeryLayout.tsx
@@ -121,7 +121,7 @@ export default function BakeryLayout({ user, onLogout }: BakeryLayoutProps) {
         </div>
       </div>
 
-      <div className="md:pl-64 flex flex-col flex-1">
+      <div className="md:pl-64 flex flex-col flex-1 min-w-0">
         {/* Top navigation */}
         <div className="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-white border-b border-gray-200">
           <button
